10ded7a2b4e5c43b90180599a20be88868159401
philcgithub <phil.cherry@gmail.com>
2021-02-15 13:16:31 +0000

    Changed to use methods from custom resource

--- cookbooks/cloudbeescd/recipes/cloudbeescd-server-express.rb
+++ cookbooks/cloudbeescd/recipes/cloudbeescd-server-express.rb
@@ -12,52 +12,27 @@ db_password = config_bag['db-password']
 admin_password = config_bag['admin-password']
 
 # Run the installer
-execute 'install-cloudbeescd-server' do
-  command "sudo #{installer_path}/#{server_installer_file} --mode silent --installServer --installWeb --installAgent --installRepository --unixServerUser #{user} --unixServerGroup #{user} --useSameServiceAccount --installDatabase --databasePassword #{db_password}"
+cloudbeescd_server 'server1' do
+  installer_path "#{installer_path}"
+  server_installer_file "#{server_installer_file}"
   user "#{user}"
-  group "#{user}"
-  action :run
+  flags "--installServer --installWeb --installAgent --installRepository --unixServerUser #{user} --unixServerGroup #{user} --useSameServiceAccount --installDatabase --databasePassword #{db_password}"
+  action :create
 end
 
 # Check server setup has finished
-ruby_block 'Wait for CloudBees CD Server setup to finish' do
-  block do
-    true until ::File.foreach('/opt/electriccloud/electriccommander/logs/setupScripts.log').grep(/Finished loading/).any?
-    # Wait between checks
-    sleep(10)
-  end
-  action :run
+cloudbeescd_server 'server1' do
+  action :waitForSetup
 end
 
 # Change admin password using ectool
-bash 'Set admin password' do
-  code <<-EOH
-    /opt/electriccloud/electriccommander/bin/ectool --server localhost login "admin" "changeme"
-    /opt/electriccloud/electriccommander/bin/ectool modifyUser "admin" --password "#{admin_password}" --sessionPassword "changeme"
-  EOH
-  action :run
+cloudbeescd_server 'server1' do
+  admin_password "#{admin_password}"
+  action :setAdminPwd
 end
 
 # Copy license file if supplied
-cookbook_file '/tmp/license.xml' do
-  source 'license.xml'
-  owner "#{user}"
-  group "#{user}"
-  action :create
-  ignore_failure true
-end
-
-# Apply license if supplied
-bash 'Apply license.xml' do
-  only_if { ::File.exist?('/tmp/license.xml') }
-  code <<-EOH
-    /opt/electriccloud/electriccommander/bin/ectool --server localhost login "admin" "#{admin_password}"
-    /opt/electriccloud/electriccommander/bin/ectool importLicenseData /tmp/license.xml
-  EOH
-  action :run
-end
-
-# Remove license file if it was copied over
-file '/tmp/license.xml' do
-  action :delete
+cloudbeescd_server 'server1' do
+  user "#{user}"
+  action :importLicense
 end
