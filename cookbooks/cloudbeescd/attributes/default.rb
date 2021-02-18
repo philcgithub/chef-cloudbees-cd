53253bd540b493be4f385b275680a2c3238abbea
philcgithub <phil.cherry@gmail.com>
2021-02-18 13:26:19 +0000

    Added database settings

--- cookbooks/cloudbeescd/attributes/default.rb
+++ cookbooks/cloudbeescd/attributes/default.rb
@@ -5,8 +5,11 @@ default['cloudbeescd']['server-installer-file'] = 'CloudBeesFlow-x64-10.0.2.1447
 default['cloudbeescd']['analytics-installer-file'] = 'CloudBeesFlowDevOpsInsightServer-x64-10.0.2.144701'
 # OS user that CloudBees CD should run as
 default['cloudbeescd']['installer-user-name'] = 'cloudbeescd'
-# Database details
-# Currently uses internal mariaDB
+# Database details if not using internal mariadb
+default['cloudbeescd']['database-name'] = 'cbcd'
+default['cloudbeescd']['database-port'] = '3306'
+default['cloudbeescd']['database-host'] = '10.154.0.5'
+default['cloudbeescd']['database-user-name'] = 'root'
 # Analytics details
 # Where analytics can reach CloudBees CD server
 default['cloudbeescd']['analytics-remoteserver'] = 'localhost'
